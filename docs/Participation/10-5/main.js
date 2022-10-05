5bbc056843b633a67a0d585ebc9e2c3e4adde0c0
Kole0518 <kdavis13@zagmail.gonzaga.edu>
2022-10-05 08:46:21 -0700

    10-5 stuff

---
+++ docs/Participation/10-5/main.js
@@ -0,0 +1,8 @@
+window.onload = function()
+{
+    let myInt = 42;
+    let myOtherInt = 90000000000;
+    anotherInt = 7; // this is fine in JS!!
+    const PI = 3.14; // immutable values
+    alert("lol. lmao.");
+}
